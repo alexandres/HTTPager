59e7f0aa8d29aae83c58d926603304c66a470e30
Alexandre T. S <outroalex@gmail.com>
2020-11-07 09:06:08 -0300

    disable csrf for page

--- pager/views.py
+++ pager/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import *
 
@@ -7,6 +8,7 @@ from .models import *
 def index(request):
     return HttpResponse("You prolly shouldn't be here.")
 
+@csrf_exempt
 def page(request, pager_slug):
     pager = Pager.objects.get(slug=pager_slug)
     pager.page(request)
